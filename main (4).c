4b0f26dec5fb1b2dfef5f6f58df7afb710933908
marti1704 <124136841+marti1704@users.noreply.github.com>
2023-02-01 12:40:08 +0100

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void ft_ft(int*nbr,int*x,int y)
+{
+    *nbr=42;
+    *x=12;
+     y=102;
+}
+int main()
+{
+   int nbr=2;
+   int x=7;
+   int y=3;
+   
+   
+   printf("il valore iniziale:%d\n",x);
+   ft_ft(&x,&nbr,y);
+   
+   printf("il valore di:%d\n",x);
+   printf("il valore di:%d\n",y);
+   printf("il valore effettivo dopo aver richiamato ft_ft:%d\n",nbr);
+   return 0;
+}

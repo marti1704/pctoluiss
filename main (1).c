8126c177bc6fcee97e4ea9283ea680fa30f8893d
marti1704 <124136841+marti1704@users.noreply.github.com>
2023-02-02 10:18:18 +0100

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int fattoriale (int numero)
+{
+     int tot=1;
+     while(numero>0)
+ {    tot=tot*numero;
+     numero=numero-1;
+ }
+      return tot;
+}
+    
+    
+int main()
+
+{
+    int N;
+    int fact;
+    printf("prendi un numero\n");  
+    scanf("%d",&N);
+    fact=fattoriale(N);
+    printf("il fattoriale e':%d\n",fact);
+   
+ return 0;
+}

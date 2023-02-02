813d2e6634f58314d6007694324ba0b94f6d99ac
marti1704 <124136841+marti1704@users.noreply.github.com>
2023-02-02 15:46:52 +0100

    Add files via upload

--- main (6).c
+++ main (6).c
@@ -8,22 +8,22 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 
-
-
-void fattoriale (int nnumero){}
-
 int main()
 {
-    int n;
-    int tot=1;
-    printf("prendi un numero\n")
-    scanf("%d\n",&numero);
-    
-    while(n>0){
-        tot=tot*n
-}
-  printf("finale %d\n",tot); 
+  
+   char str[]="ciao";
+   int i=0;
    
-   return 0;
-
+    
+    while(str[i]!='\0')
+    {
+        i=i+1;
+    }
+    while(i>0)
+    {
+        i=i-1;
+        printf("%c",str[i]);
+    }
+   
+    return 0;
 }

d4ea58c7584f64fc23367d26394b8d8e4a4193db
marti1704 <124136841+marti1704@users.noreply.github.com>
2023-02-03 12:35:58 +0100

    Add files via upload

--- main (1).c
+++ main (1).c
@@ -1,33 +1,53 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
-int fattoriale (int numero)
+void ft_ft(int  *array, int dimensione )
 {
-     int tot=1;
-     while(numero>0)
- {    tot=tot*numero;
-     numero=numero-1;
- }
-      return tot;
-}
-    
-    
-int main()
+    int i;
+    int temp;
+    int z;
 
-{
-    int N;
-    int fact;
-    printf("prendi un numero\n");  
-    scanf("%d",&N);
-    fact=fattoriale(N);
-    printf("il fattoriale e':%d\n",fact);
+    temp=0;
+    i=0;
    
- return 0;
+ while(i<dimensione-1) 
+ { 
+    z=i+1;
+     while(z<dimensione)
+     {
+    if(array[i]>array[z])
+    {
+       temp=array[i];
+       array[i]=array[z];
+       array[z]=temp;
+    }
+    z=z+1;
+ }
+   i=i+1;
+ }  
+}
+
+int main()
+{
+   
+    int array[]={7,2,5,9,4,1,3};
+    int dimensione=7;
+    int i=0;
+    
+    ft_ft(array,dimensione);
+    while(i<dimensione)
+    {
+        printf("%d\n",array[i]);
+        i=i+1;
+    }
+  
+
+    return 0;
 }
